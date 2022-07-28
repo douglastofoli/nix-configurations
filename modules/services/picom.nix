ea452a6e8d1529279eb90488dc07ca4826f1b359
douglastofoli <tofoli.douglas@hotmail.com>
2022-07-27 21:51:26 -0300

    fix

--- modules/services/picom.nix
+++ modules/services/picom.nix
@@ -3,14 +3,6 @@
 { 
   services.picom = {
     enable = true;
-    package = pkgs.picom.overrideAttrs(o: {
-      src = pkgs.fetchFromGitHub {
-        repo = "picom";
-        owner = "ibhagwan";
-        rev = "c4107bb6cc17773fdc6c48bb2e475ef957513c7a";
-        sha256 = "4voCAYd0fzJHQjJo4x3RoWz5l3JJbRvgIXn1Kg6nz6Y=";
-      };
-    });                                           # Override picom to use pijulius' version
 
     backend = "glx";                              # Rendering either with glx or xrender. You'll know if you need to switch this.
     vSync = true;                                 # Should fix screen tearing
